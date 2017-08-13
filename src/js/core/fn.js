7109507728a29b7d229e3da7d8ddd8368be42dd5
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2017-08-13 12:16:37 +0430

    typo

--- src/js/core/fn.js
+++ src/js/core/fn.js
@@ -1,12 +1,12 @@
 define(() => {
 
 	function determineHost() {
-		let hName = window.location.hostname;
+		const hName = window.location.hostname;
 		let res;
 		if (hName) {	
 			if (hName === "localhost" || hName === "127.0.0.1") {
 				res = `${hName}:1081`;
-			} else if (host === "80.85.82.83" || hName === "10.10.200.64") {
+			} else if (hName === "80.85.82.83" || hName === "10.10.200.64") {
 				res = `${hName}:8081`;
 			}
 		}
